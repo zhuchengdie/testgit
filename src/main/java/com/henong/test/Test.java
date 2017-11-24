415ff0bb9ac888ad9f43b433e6d282c8c80870a1
DESKTOP-IQJ2PGJ\user <290518098@qq.com>
2017-11-24 16:21:38 +0800

    hello world2

--- src/main/java/com/henong/test/Test.java
+++ src/main/java/com/henong/test/Test.java
@@ -5,6 +5,6 @@ package com.henong.test;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!22");
     }
 }
