99bae44e8f709d04112d145df9c05ed0eed21c38
DESKTOP-IQJ2PGJ\user <290518098@qq.com>
2017-11-24 16:18:12 +0800

    hello world

---
+++ src/main/java/com/henong/test/Test.java
@@ -0,0 +1,10 @@
+package com.henong.test;
+
+/**
+ * Created by user on 2017/11/24.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
