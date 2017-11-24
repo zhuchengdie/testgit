2fb2f516014f08d5cc559cd409748852fcc87530
DESKTOP-IQJ2PGJ\user <290518098@qq.com>
2017-11-24 16:47:54 +0800

    branch1

--- src/main/java/com/henong/test/Branch.java
+++ src/main/java/com/henong/test/Branch.java
@@ -4,4 +4,7 @@ package com.henong.test;
  * Created by user on 2017/11/24.
  */
 public class Branch {
+    public static void main(String[] args) {
+        System.out.println("branchzcd1");
+    }
 }
